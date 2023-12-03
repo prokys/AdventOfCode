481b88f7d6312f29870d55c693f139c7f09f7657
Prokop Ferdan <prokes1@gmail.com>
2023-12-03 22:09:20 +0100

    added first day part two solution

--- src/day1/Day1.java
+++ src/day1/Day1.java
@@ -13,11 +13,32 @@ public class Day1 {
     public static void main(String args[]){
         readFileAndStoreValues();
         String[] inputArray = inputArrayList.toArray(new String[0]);
+//        System.out.println(inputArray[0]);
+//        findNumbersInText(inputArray);
+//        System.out.println(inputArray[0]);
         reduceArrayToNumbers(inputArray);
         String[] numbersArray = numbersArrayList.toArray(new String[0]);
+//        System.out.println(numbersArray[0]);
         reduceArrayToTwoNumbers(numbersArray);
         String[] twoNumbersArray = twoNumbersArrayList.toArray(new String[0]);
+//        System.out.println(twoNumbersArray[0]);
+//        System.out.println(Integer.parseInt(twoNumbersArray[0]));
+//        printArray(twoNumbersArray);
         printFinalNumber(twoNumbersArray);
+    }
+
+    public static void findNumbersInText(String[] array){
+        for (int i=0; i<array.length; i++){
+            array[i] = array[i].replaceAll("one", "1");
+            array[i] = array[i].replaceAll("two", "2");
+            array[i] = array[i].replaceAll("three", "3");
+            array[i] = array[i].replaceAll("four", "4");
+            array[i] = array[i].replaceAll("five", "5");
+            array[i] = array[i].replaceAll("six", "6");
+            array[i] = array[i].replaceAll("seven", "7");
+            array[i] = array[i].replaceAll("eight", "8");
+            array[i] = array[i].replaceAll("nine", "9");
+        }
     }
     public static void readFileAndStoreValues(){
         try{
@@ -37,9 +58,65 @@ public class Day1 {
             var asd = new StringBuilder(value);
             StringBuilder stringBuilder = new StringBuilder();
             for (int y=0; y<asd.length();y++){
-                if(Character.isDigit(asd.charAt(y))){
-                    stringBuilder.append(asd.charAt(y));
-                }
+                try  {
+                    if(Character.isDigit(asd.charAt(y))) {
+                        stringBuilder.append(asd.charAt(y));
+                    } else if (Character.isLetter(asd.charAt(y)) &&
+                            (Character.compare(asd.charAt(y), 'o') == 0) &&
+                            (Character.compare(asd.charAt(y + 1), 'n') == 0) &&
+                            (Character.compare(asd.charAt(y + 2), 'e') == 0)) {
+                        stringBuilder.append("1");
+                    }else if (Character.isLetter(asd.charAt(y)) &&
+                            (Character.compare(asd.charAt(y), 't') == 0) &&
+                            (Character.compare(asd.charAt(y + 1), 'w') == 0) &&
+                            (Character.compare(asd.charAt(y + 2), 'o') == 0)) {
+                        stringBuilder.append("2");
+                    }else if (Character.isLetter(asd.charAt(y)) &&
+                            (Character.compare(asd.charAt(y), 't') == 0) &&
+                            (Character.compare(asd.charAt(y + 1), 'h') == 0) &&
+                            (Character.compare(asd.charAt(y + 2), 'r') == 0) &&
+                            (Character.compare(asd.charAt(y + 3), 'e') == 0)&&
+                            (Character.compare(asd.charAt(y + 4), 'e') == 0))  {
+                        stringBuilder.append("3");
+                    }else if (Character.isLetter(asd.charAt(y)) &&
+                            (Character.compare(asd.charAt(y), 'f') == 0) &&
+                            (Character.compare(asd.charAt(y + 1), 'o') == 0) &&
+                            (Character.compare(asd.charAt(y + 2), 'u') == 0) &&
+                            (Character.compare(asd.charAt(y + 3), 'r') == 0)){
+                        stringBuilder.append("4");
+                    }else if (Character.isLetter(asd.charAt(y)) &&
+                            (Character.compare(asd.charAt(y), 'f') == 0) &&
+                            (Character.compare(asd.charAt(y + 1), 'i') == 0) &&
+                            (Character.compare(asd.charAt(y + 2), 'v') == 0) &&
+                            (Character.compare(asd.charAt(y + 3), 'e') == 0)){
+                        stringBuilder.append("5");
+                    }else if (Character.isLetter(asd.charAt(y)) &&
+                            (Character.compare(asd.charAt(y), 's') == 0) &&
+                            (Character.compare(asd.charAt(y + 1), 'i') == 0) &&
+                            (Character.compare(asd.charAt(y + 2), 'x') == 0)){
+                        stringBuilder.append("6");
+                    }else if (Character.isLetter(asd.charAt(y)) &&
+                            (Character.compare(asd.charAt(y), 's') == 0) &&
+                            (Character.compare(asd.charAt(y + 1), 'e') == 0) &&
+                            (Character.compare(asd.charAt(y + 2), 'v') == 0) &&
+                            (Character.compare(asd.charAt(y + 2), 'v') == 0) &&
+                            (Character.compare(asd.charAt(y + 4), 'n') == 0)){
+                        stringBuilder.append("7");
+                    }else if (Character.isLetter(asd.charAt(y)) &&
+                            (Character.compare(asd.charAt(y), 'e') == 0) &&
+                            (Character.compare(asd.charAt(y + 1), 'i') == 0) &&
+                            (Character.compare(asd.charAt(y + 2), 'g') == 0) &&
+                            (Character.compare(asd.charAt(y + 3), 'h') == 0) &&
+                            (Character.compare(asd.charAt(y + 4), 't') == 0)){
+                        stringBuilder.append("8");
+                    }else if (Character.isLetter(asd.charAt(y)) &&
+                            (Character.compare(asd.charAt(y), 'n') == 0) &&
+                            (Character.compare(asd.charAt(y + 1), 'i') == 0) &&
+                            (Character.compare(asd.charAt(y + 2), 'n') == 0) &&
+                            (Character.compare(asd.charAt(y + 3), 'e') == 0) ){
+                        stringBuilder.append("9");
+                    }
+                }catch (Exception ignore){}
             }
             numbersArrayList.add(stringBuilder.toString());
         }
