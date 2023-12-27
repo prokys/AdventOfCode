3c5f19346a94e7b3796bf372373c482ff5945c3c
Prokop Ferdan <prokes1@gmail.com>
2023-12-27 22:14:22 +0100

    added day 2 part 2 solution

--- src/day2/Day2.java
+++ src/day2/Day2.java
@@ -7,7 +7,8 @@ import java.util.ArrayList;
 public class Day2 {
     public static ArrayList<String> inputArrayList = Utils.readFileAndStoreValues("src\\day2\\input.txt");
     public static ArrayList<Integer> gamePositionsList = new ArrayList<Integer>();
-    public static int number=0;
+    public static int partOneNumber =0;
+    public static int partTwoNumber =0;
 
     public static void main(String args[]){
         String[] inputArray = inputArrayList.toArray(new String[0]);
@@ -16,7 +17,9 @@ public class Day2 {
         Integer[] positionsArray = gamePositionsList.toArray(new Integer[0]);
 //        Utils.printArray(inputArray);
         checkColors(inputArray, positionsArray);
-        System.out.println(number);
+        countSumOfMinimumCubesSetPower(inputArray, positionsArray);
+        System.out.println(partOneNumber);
+        System.out.println(partTwoNumber);
 
 
     }
@@ -51,8 +54,31 @@ public class Day2 {
             }
 
             if(red == true && blue == true && green == true){
-                number = number + (Integer.parseInt(array[(positionArray[i-1])+1].replaceAll(":","")));
+                partOneNumber = partOneNumber + (Integer.parseInt(array[(positionArray[i-1])+1].replaceAll(":","")));
             }
+        }
+    }
+    public static void countSumOfMinimumCubesSetPower(String array[], Integer positionArray[]){
+        for (int i = 1; i<positionArray.length; i++){
+            int red = 0;
+            int blue = 0;
+            int green = 0;
+            for (int y=positionArray[i-1]; y<positionArray[i]; y++ ){
+                if (array[y].startsWith("red")){
+                    if (Integer.parseInt(array[y-1])> red){
+                        red = Integer.parseInt(array[y - 1]);
+                    }
+                } else if (array[y].startsWith("blue")) {
+                    if (Integer.parseInt(array[y - 1]) > blue) {
+                        blue = Integer.parseInt(array[y - 1]);
+                    }
+                }else if (array[y].startsWith("green")) {
+                    if (Integer.parseInt(array[y - 1]) > green) {
+                        green = Integer.parseInt(array[y - 1]);
+                    }
+                }
+            }
+                partTwoNumber = partTwoNumber + (red*blue*green);
         }
     }
 }
