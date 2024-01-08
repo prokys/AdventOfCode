2fb2f70ea67ab348622680f3a318de84724c44b1
Prokop Ferdan <prokes1@gmail.com>
2024-01-08 22:36:48 +0100

    added day 3 part 2 solution

--- src/day3/Day3.java
+++ src/day3/Day3.java
@@ -7,8 +7,61 @@ import java.util.ArrayList;
 public class Day3 {
     public static ArrayList<String> inputArrayList = Utils.readFileAndStoreValues("src\\day3\\input.txt");
     public static int finalNumber= 0;
+    public static int finalNumberPartTwo = 0;
     public static void main(String args[]){
         partOne();
+        partTwo();
+    }
+
+    public static void partTwo(){
+        String inputArray[] = inputArrayList.toArray(new String[0]);
+        for(int y=0;y<inputArray.length;y++) {
+            StringBuilder stringBuilder = new StringBuilder(inputArray[y]);
+            for (int i = 0; i < stringBuilder.length(); i++) {
+                if (stringBuilder.charAt(i)=='*') {
+                    ArrayList<String> numbersAroundStar= new ArrayList<>();
+                    for (int z = i - 1; z <= i + 1; z++) {
+                        StringBuilder number = new StringBuilder();
+                        for (int c = y - 1; c <= y + 1; c++) {
+                            if (z >= 0 && c >= 0 && z < stringBuilder.length() && c < inputArray.length) {
+                                StringBuilder stringToCheck = new StringBuilder(inputArray[c]);
+                                if (z < stringToCheck.length() - 2 && Character.isDigit(stringToCheck.charAt(z)) && Character.isDigit(stringToCheck.charAt(z + 1)) && Character.isDigit(stringToCheck.charAt(z + 2))) {
+                                    number.append(stringToCheck.charAt(z));
+                                    number.append(stringToCheck.charAt(z + 1));
+                                    number.append(stringToCheck.charAt(z + 2));
+                                    numbersAroundStar.add(number.toString());
+                                    number.delete(0,3);
+                                }else if (z > 0 && i < stringToCheck.length() - 1 && !Character.isDigit(stringToCheck.charAt(z - 1)) && Character.isDigit(stringToCheck.charAt(z)) && Character.isDigit(stringToCheck.charAt(z + 1)) && !Character.isDigit(stringToCheck.charAt(z + 2))) {
+                                    number.append(stringToCheck.charAt(z));
+                                    number.append(stringToCheck.charAt(z + 1));
+                                    numbersAroundStar.add(number.toString());
+                                    number.delete(0,2);
+                                }else if (z > 0 && !Character.isDigit(stringToCheck.charAt(z - 1)) && Character.isDigit(stringToCheck.charAt(z)) && !Character.isDigit(stringToCheck.charAt(z + 1))) {
+                                    number.append(stringToCheck.charAt(z));
+                                    numbersAroundStar.add(number.toString());
+                                    number.delete(0,1);
+                                }else if (((z==i-1) ||(z==i)) && Character.isDigit(stringToCheck.charAt(z)) && Character.isDigit(stringToCheck.charAt(z-1)) && Character.isDigit(stringToCheck.charAt(z - 2))) {
+                                    number.append(stringToCheck.charAt(z - 2));
+                                    number.append(stringToCheck.charAt(z - 1));
+                                    number.append(stringToCheck.charAt(z));
+                                    numbersAroundStar.add(number.toString());
+                                    number.delete(0,3);
+                                }else if (z ==i-1 && !Character.isDigit(stringToCheck.charAt(z + 1)) && Character.isDigit(stringToCheck.charAt(z)) && Character.isDigit(stringToCheck.charAt(z - 1)) && !Character.isDigit(stringToCheck.charAt(z - 2))) {
+                                    number.append(stringToCheck.charAt(z - 1));
+                                    number.append(stringToCheck.charAt(z));
+                                    numbersAroundStar.add(number.toString());
+                                    number.delete(0,2);
+                                }
+                            }
+                        }
+                    }
+                    if(numbersAroundStar.size() == 2){
+                        finalNumberPartTwo = finalNumberPartTwo + (Integer.parseInt(numbersAroundStar.get(0)) * Integer.parseInt(numbersAroundStar.get(1)));
+                    }
+                }
+            }
+        }
+        System.out.println(finalNumberPartTwo);
 
     }
     public static void partOne(){
