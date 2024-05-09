7db49a86e12263e37f5fcd272624bc356e52f848
Prokop Ferdan <prokes1@gmail.com>
2024-05-09 18:44:17 +0200

    working on day 5 part 2

--- src/day5/Day5.java
+++ src/day5/Day5.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 
 public class Day5 {
     public static ArrayList<String> inputArrayList = Utils.readFileAndStoreValues("src\\day5\\input.txt");
-    private static int seed = inputArrayList.indexOf("seeds:");;
+    private static int seed = inputArrayList.indexOf("seeds:");
     private static int seedToSoil = inputArrayList.indexOf("seed-to-soil");
     private static  int soilToFertilizer= inputArrayList.indexOf("soil-to-fertilizer");
     private static int fertilizerToWater= inputArrayList.indexOf("fertilizer-to-water");
@@ -15,46 +15,63 @@ public class Day5 {
     private static int temperatureToHumidity= inputArrayList.indexOf("temperature-to-humidity");
     private static int humidityToLocation= inputArrayList.indexOf("humidity-to-location");
 
-    private static long seedValue;
     private static long finalNumber;
 
     private static ArrayList<Long> locations = new ArrayList<Long>();
 
     public static void main(String[] args) {
-        partOne();
+//        partOne();
+        partTwo();
 
     }
 
     private static void partOne(){
         for (int i = seed+1; i<seedToSoil; i++){
-            seedValue = Long.parseLong(inputArrayList.get(i));
-            getLocation();
+            locations.add(getLocation(Long.parseLong(inputArrayList.get(i))));
         }
         getFinalNumber();
         System.out.println(finalNumber);
     }
 
-    private static void getSeedValue(int from, int to, long seedToCompare){
+    private static void partTwo(){
+        for (int i = seed+1; i<seedToSoil; i+=2){
+            long seedValue = Long.parseLong(inputArrayList.get(i));
+            long seedRange = Long.parseLong(inputArrayList.get(i+1));
+
+                for (long y = seedValue; y<(seedValue+seedRange); y++){
+                    locations.add(getLocation(y));
+            }
+
+
+        }
+        getFinalNumber();
+        System.out.println(finalNumber);
+    }
+
+    private static long getSeedValue(int from, int to, long seedToCompare){
+        long seedToLocationValue = seedToCompare;
         for (int i =from+3; i<to; i+=3){
             long base = Long.parseLong(inputArrayList.get(i));
             long range = Long.parseLong(inputArrayList.get(i+1));
             long mapValue = Long.parseLong(inputArrayList.get(i-1));
 
             if (seedToCompare>=base && seedToCompare<(base+range)){
-                seedValue = seedValue-base+mapValue;
+                seedToLocationValue = seedToLocationValue -base+mapValue;
                 break;
             }
         }
+        return seedToLocationValue;
     }
-    private static void getLocation(){
-        getSeedValue(seedToSoil, soilToFertilizer, seedValue);
-        getSeedValue(soilToFertilizer, fertilizerToWater, seedValue);
-        getSeedValue(fertilizerToWater, waterToLight, seedValue);
-        getSeedValue(waterToLight, lightToTemperature, seedValue);
-        getSeedValue(lightToTemperature, temperatureToHumidity, seedValue);
-        getSeedValue(temperatureToHumidity, humidityToLocation, seedValue);
-        getSeedValue(humidityToLocation, inputArrayList.size(), seedValue);
-        locations.add(seedValue);
+    private static long getLocation(long seedToLocationBaseValue){
+        long seedToLocationValue = seedToLocationBaseValue;
+        seedToLocationValue = getSeedValue(seedToSoil, soilToFertilizer, seedToLocationValue);
+        seedToLocationValue = getSeedValue(soilToFertilizer, fertilizerToWater, seedToLocationValue);
+        seedToLocationValue = getSeedValue(fertilizerToWater, waterToLight, seedToLocationValue);
+        seedToLocationValue = getSeedValue(waterToLight, lightToTemperature, seedToLocationValue);
+        seedToLocationValue = getSeedValue(lightToTemperature, temperatureToHumidity, seedToLocationValue);
+        seedToLocationValue = getSeedValue(temperatureToHumidity, humidityToLocation, seedToLocationValue);
+        seedToLocationValue = getSeedValue(humidityToLocation, inputArrayList.size(), seedToLocationValue);
+        return seedToLocationValue;
     }
 
     public static void getFinalNumber() {
