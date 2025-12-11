df8908cc19f0b1bfe1484abea75344bdc6286151
Prokop Ferdan <prokes1@gmail.com>
2025-12-11 09:10:50 +0100

    added soltuion to day2 part two

--- src/main/java/AOC2025/day2/Day2.java
+++ src/main/java/AOC2025/day2/Day2.java
@@ -36,6 +36,68 @@ public class Day2 {
         return finalNumber;
     }
 
+public static long partTwo(String input){
+    long finalNumber = 0;
+    List<String> inputList = splitInput(input);
+
+    for (int i = 0; i < inputList.size()-1; i+=2) {
+        long num1 = Long.parseLong(inputList.get(i));
+        long num2 = Long.parseLong(inputList.get(i+1));
+
+
+        for (long j = num1; j <= num2; j++) {
+            String string = String.valueOf(j);
+            int stringLength = string.length();
+            boolean counted = false;
+
+            if (stringLength > 1){
+                if (isSequence(string, 1)){
+                    finalNumber = finalNumber + j;
+                    counted = true;
+                }
+            }
+            if ((stringLength == 4 || stringLength == 6 || stringLength == 8 || stringLength == 10) && !counted){
+                if (isSequence(string, 2)){
+                    finalNumber = finalNumber + j;
+                    counted = true;
+                }
+            }
+            if ((stringLength == 6 || stringLength == 9) && !counted){
+                if (isSequence(string, 3)){
+                    finalNumber = finalNumber + j;
+                    counted = true;
+                }
+            }
+            if (stringLength == 8 && !counted){
+                if (isSequence(string, 4)){
+                    finalNumber = finalNumber + j;
+                    counted = true;
+                }
+            }
+            if (stringLength == 10 && !counted){
+                if (isSequence(string, 5)){
+                    finalNumber = finalNumber + j;
+                }
+            }
+
+        }
+    }
+
+    System.out.println(finalNumber);
+    return finalNumber;
+    }
+
+    private static boolean isSequence(String input, int length) {
+        String part = input.substring(0,length);
+        for (int i = length; i < input.length(); i+=length) {
+            String compare = input.substring(i,i+length);
+            if (!part.equals(compare)){
+                return false;
+            }
+        }
+        return true;
+    }
+
     public static List<String> splitInput(String input){
         List<String> inputList = new ArrayList<>();
         StringBuilder stringBuilder = new StringBuilder();
--- src/test/java/AOC2025/day2/Day2Test.java
+++ src/test/java/AOC2025/day2/Day2Test.java
@@ -10,6 +10,7 @@ import utils.Utils;
 import java.util.stream.Stream;
 
 import static AOC2025.day2.Day2.partOne;
+import static AOC2025.day2.Day2.partTwo;
 
 public class Day2Test {
 
@@ -33,5 +34,31 @@ public class Day2Test {
     @MethodSource("data")
     public void testPartOne(String input, long result){
         Assertions.assertEquals(result, partOne(input));
+    }
+
+
+    static Stream<Arguments> dataTwo() {
+        String input = Utils.readFileAndStoreValues("src/main/java/AOC2025/day2/input.txt").get(0);
+        return Stream.of(
+                Arguments.of("100-999,", 4995),
+                Arguments.of("11-22,", 33),
+                Arguments.of("95-115,", 210),
+                Arguments.of("998-1012,", 2009),
+                Arguments.of("1188511880-1188511890,", 1188511885),
+                Arguments.of("222220-222224,", 222222),
+                Arguments.of("1698522-1698528,", 0),
+                Arguments.of("446443-446449,", 446446),
+                Arguments.of("38593856-38593862,", 38593859),
+                Arguments.of("565653-565659,", 565656),
+                Arguments.of("824824821-824824827,", 824824824),
+                Arguments.of("2121212118-2121212124,", 2121212121),
+                Arguments.of("11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124,", 4174379265L),
+                Arguments.of(input, 43872163557L)
+        );
+    }
+    @ParameterizedTest
+    @MethodSource("dataTwo")
+    public void testPartTwo(String input, long result){
+        Assertions.assertEquals(result, partTwo(input));
     }
 }
